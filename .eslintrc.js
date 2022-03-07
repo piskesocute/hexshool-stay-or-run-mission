a9c8527db5bdf1e9d26201d217368de607f9b947
Evan Chiang <happyday9837@gmail.com>
2022-03-08 02:49:01 +0800

    create-Create google font

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'linebreak-style': [
+      'off',
+      'windows',
+    ],
+    'no-console': 'off',
+  },
+};
